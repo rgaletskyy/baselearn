1ef527ae03554b47048e067ac22b922b5c5bbb7a
Roman <82839606+rgaletskyy@users.noreply.github.com>
2024-10-06 10:59:13 +0300

    Update GarageManager.sol

--- GarageManager.sol
+++ GarageManager.sol
@@ -2,57 +2,55 @@
 pragma solidity ^0.8.17;
 
 contract GarageManager{
-    struct Car {
+    struct Car {        
         string make;
         string model;
         string color;
-        uint8 numberOfDoors;
+        uint8 numberOfDoors;       
     }
 
     mapping (address => Car[]) public garage;
 
-    function addCar(string calldata _make, string calldata _model, string calldata _color, uint8 _numberOfDoors) public {
-        
-        Car memory newCar = Car(
-            {
-                make: _make,
-                model: _model,
-                color: _color,
-                numberOfDoors: _numberOfDoors
-            }
-        );       
+    error BadCarIndex(uint index);
+
+    // Function to add a car to the user's collection in the garage
+    function addCar(string calldata _make, string calldata _model, string calldata _color, uint8 _numberOfDoors) external {
+        // Create a new car and push it to the user's garage
+        Car memory newCar = Car({
+            make: _make,
+            model: _model,
+            color: _color,
+            numberOfDoors: _numberOfDoors
+        });
 
         garage[msg.sender].push(newCar);
     }
 
-    function getMyCars() public view returns (Car[] memory myCars) {
-        myCars = garage[msg.sender];
-
-        return myCars;
+    function getMyCars() external view returns (Car[] memory) {
+        return garage[msg.sender];
     }
 
-    function getUserCars(address userAddress) public view returns (Car[] memory userCars) {
-        userCars = garage[userAddress];
-
-        return userCars;
+    function getUserCars(address _user) external view returns (Car[] memory) {
+        return garage[_user];
     }
 
-    error BadCarIndex(uint index);
-    function updateCar(uint index, uint8 _numberOfDoors, string memory _make, string memory _model, string memory _color) public {
-        Car[] memory myCars = garage[msg.sender];
-
-        if(myCars.length < index) {
-            revert BadCarIndex(index);
+    
+    // Function to update a car's details for the calling user
+    function updateCar(uint _index, string calldata _make, string calldata _model, string calldata _color, uint8 _numberOfDoors) external {
+        // Check if the car exists for the calling user
+        if (_index >= garage[msg.sender].length) {
+            revert BadCarIndex(_index);
         }
 
-        Car memory carToUpdate = myCars[index];
+        // Update the car at the given index
+        Car storage carToUpdate = garage[msg.sender][_index];
         carToUpdate.make = _make;
         carToUpdate.model = _model;
         carToUpdate.color = _color;
         carToUpdate.numberOfDoors = _numberOfDoors;
     }
 
-    function resetMyGarage() public {
+    function resetMyGarage() external {
         delete garage[msg.sender];
     }
 
